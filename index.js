fbc7f0e81a5d46edb465ff40437c849812f833ca
David Hérault <dherault@gmail.com>
2022-12-05 23:58:24 +0100

    Create waita

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+const [rawDelay] = process.argv.slice(2)
+
+if (!rawDelay) {
+  console.error('Usage: "waita <delay in ms>" (You omitted the delay argument)');
+  process.exit(1); //an error occurred
+}
+
+const delay = parseInt(rawDelay)
+
+if (delay !== delay) {
+  console.error('Usage: "waita <delay in ms>" (The provided delay argument is not a number)');
+  process.exit(1); //an error occurred
+}
+
+waita(delay)
+
+async function waita(delay) {
+  await new Promise(resolve => setTimeout(resolve, delay))
+}
