ec278f91c7dd7d41f3275f4bb6f75b0d09fda422
David Hérault <dherault@gmail.com>
2022-12-06 03:26:14 +0100

    Lint

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 #! /usr/bin/env node
-
 const [rawDelay] = process.argv.slice(2)
 
 if (!rawDelay) {
-  console.error('Usage: "waita <delay in ms>" (You omitted the delay argument)');
-  process.exit(1); //an error occurred
+  console.error('Usage: "waita <delay in ms>" (You omitted the delay argument)')
+  process.exit(1)
 }
 
 const delay = parseInt(rawDelay)
 
 if (delay !== delay) {
-  console.error('Usage: "waita <delay in ms>" (The provided delay argument is not a number)');
-  process.exit(1); //an error occurred
+  console.error('Usage: "waita <delay in ms>" (The provided delay argument is not a number)')
+  process.exit(1)
 }
 
 waita(delay)
